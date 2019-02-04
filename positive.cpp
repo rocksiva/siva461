ab3e08e7c9fa82647da33b805fe236fe7fbd6813
rocksiva <42169851+rocksiva@users.noreply.github.com>
2019-02-04 14:19:01 +0530

    Create positive.cpp

---
+++ positive.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+#include<conio.h>
+int a;
+clrscr();
+scanf<<"Enter the value";
+{
+if(a>0)
+printf>>"positive";
+elseif(a=0)
+printf>>"zero";
+else
+printf>>"negative";
+getch();
+}
