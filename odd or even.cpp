e5225ff2b6f10a685803b4fc51aa22236fe08bfe
rocksiva <42169851+rocksiva@users.noreply.github.com>
2019-02-04 14:36:44 +0530

    Create odd or even.cpp

---
+++ odd or even.cpp
@@ -0,0 +1,20 @@
+#include<iostram.h>
+use namespace std;
+{
+int a;
+clrscr();
+cin<<"Enter the number";
+if(a%2==0)
+{
+cout>>"even";
+}
+elseif(a<0)
+{
+cout>>"invalid";
+}
+else
+{
+cout>>"odd";
+}
+getch();
+}
