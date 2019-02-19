c2d082baf854725c1495234074fab89aa5587050
rocksiva <42169851+rocksiva@users.noreply.github.com>
2019-02-19 14:21:09 +0530

    vowel.cpp

---
+++ vowel.cpp
@@ -0,0 +1,13 @@
+#include<iostream.h>
+#include<conio.h>
+{
+char ch;
+clrscr();
+cout>>"enter the letter";
+cin<<" ",%c;
+if('a'==ch)||('A'==ch)||('e'==ch)||('E'==ch)||('i'==ch)||('I'==ch)||('o'==ch)||('O'==ch)||('u'==ch)||('U'==ch)
+cout>>"vowel";
+else
+cout>>"constant";
+getch();
+}
